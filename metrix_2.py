9d095c6e331bc904a75ac8ea74e9e0acec244293
avi3165 <avi316532068@gmail.com>
2025-06-20 00:06:36 +0300

    add q2

--- metrix_2.py
+++ metrix_2.py
@@ -1,9 +1,11 @@
 #q1
-list_1=[]
-num=int(input("Please enter a number: "))
-while num !=0:
-    list_1.append(num)
-    num = int(input("Please enter a number: "))
-a=list_1.pop(-1)
-list_1.insert(0,a)
-print(list_1)
+# list_1=[]
+# num=int(input("Please enter a number: "))
+# while num !=0:
+#     list_1.append(num)
+#     num = int(input("Please enter a number: "))
+# a=list_1.pop(-1)
+# list_1.insert(0,a)
+# print(list_1)
+#q2
+#answer=[5,8,9,8,5]
