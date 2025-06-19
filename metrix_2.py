018e06e9e4453c72ffcb8c08f468a6aba94cc435
avi3165 <avi316532068@gmail.com>
2025-06-20 00:07:17 +0300

    add q3

--- metrix_2.py
+++ metrix_2.py
@@ -9,3 +9,20 @@
 # print(list_1)
 #q2
 #answer=[5,8,9,8,5]
+# q3
+# arr1 = [5,4,3]
+# arr2 = [1,10,-5]
+# arr3 = [10, 30, 15, 2]
+# arr4 = [200, 20]
+# All=[arr1,arr2,arr3,arr4]
+# print(len(All))
+# chek=[3,44,10,5,6]
+# for num in chek:
+#     x=0
+#     for b_i,b_v in enumerate(All):
+#         if num in b_v:
+#             print(num,"in All",b_i)
+#             x=1
+#             break
+#     if x==0:
+#         print(num, 'not in All',b_i)
